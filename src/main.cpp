45f3cf09480f0b9231d9bb7220ce9265e20f1ab4
elaye <genard.elie@orange.fr>
2015-05-16 15:58:50 +0100

    Add OpenGL version

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4,3);
+	settings.width = 1024;
+	settings.height = 768;
+	settings.windowMode = OF_WINDOW;
+	ofCreateWindow(settings);
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
 	ofRunApp(new ofApp());
-
 }
